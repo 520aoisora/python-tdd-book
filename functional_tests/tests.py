660c670a41dc4e055859297e4b24418119186c12
520aoisora <520aoisora@gmail.com>
2018-01-29 23:18:38 +0000

    Create second FT to test multi user and unique URL

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -28,7 +28,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Weijia has heard about a cool new online to-do app.
         # She goes to check out its homepage
         self.browser.get(self.live_server_url)
@@ -60,20 +60,54 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
+        # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table('1: Buy peacock feather')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-        # There is still a text box inviting her to add another item.
-        # She enters "Use peacock feathers to make a fly"
-        # (Weijia is very methodical)
-        self.fail('Finish the test!')
-
-        # The page updates again, and now shows both items on her list
-
-        # Weijia wonders whether the site will remember her list.
-        # The she sees that the site has generated a unique URL for her
-        # -- there is some explanatory text to that effect.
-
-        # She visits that URL - her to-do list is still there.
-
         # Satisfied, she goes back to sleep
+
+    def test_multiple_users_can_start_lists_at_different_ruls(self):
+        # Weijia starts a new to-do list
+        self.browser.get(self.live_server_url)
+        self.browser.get(self.live_server_url)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # She notices that her list has a unique URL
+        weijia_list_url = self.browser.current_url
+        self.assertRegex(weijia_list_url, '/lists/.+')
+
+        # Now a new user, Yi, comes along to the site.
+
+        # # We use a new browser session to make sure that no information
+        # # of Weijia's is coming through from cookies etc
+        self.browser.quit()
+        self.browser.webdriver.Firefox()
+
+        # Yi visits the home page. There is no sign of Weijia's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy Peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Yi starts a new list by entering a new item.
+        # He is less interesting than Weijia ...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Yi gets his own unique URL
+        yi_list_url = self.browser.current_url
+        self.assertRegex(yi_list_url, 'lists/.+')
+        self.assertNotEqual(weijia_list_url, yi_list_url)
+
+        # Again, there is no trace of Weijia's list
+        page_text = self.browser.find_element_by_tag_name('body').add_context
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep
